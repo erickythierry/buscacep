61fc7908c3c41f4e4317accc2d794f3da952773f
Ericky Thierry <erickythierry@gmail.com>
2020-12-02 11:37:12 -0300

    add botao limpar

--- ajax.js
+++ ajax.js
@@ -12,13 +12,16 @@ function buscaCep() {
 		}
 	}
 	xhr.send();
+	var btnlimpar = document.getElementById("limpar");
+	btnlimpar.style.display = "inline";
+	  
 }
 
 function preencheCampos(json) {
 
 	var elemento_pai = document.getElementById('centro');
 	var conteudo = document.createElement('textarea');
-	conteudo.setAttribute("id", "resultado");
+	conteudo.setAttribute("class", "resultado");
 	conteudo.setAttribute("rows", "15");
 	conteudo.setAttribute("cols", "50");
 	var texto = document.createTextNode(json);
@@ -26,4 +29,15 @@ function preencheCampos(json) {
 	elemento_pai.appendChild(conteudo);
 
 
+}
+
+function limpar(){
+	let textos = document.getElementsByClassName("resultado");
+	if (textos.length <=1 ) {
+		var btnlimpar = document.getElementById("limpar");
+		btnlimpar.style.display = "none";
+	}
+	for(var i=0; i<textos.length; i++) {
+		textos[-i].remove();
+	}
 }
